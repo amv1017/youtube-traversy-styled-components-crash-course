aa4a9be9e2753889efcf2d21fe02d2fb9b7d9886
amv1017 <Vinokurov@bbs-c.ru>
2022-01-12 23:32:04 +0300

    footer and social icons

---
+++ src/components/styles/SocialIcons.styled.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const StyledSocialIcons = styled.div`
+  align-items: center;
+  display: flex;
+  justify-content: center;
+
+  li {
+    list-style: none;
+  }
+
+  a {
+    align-items: center;
+    border-radius: 50%;
+    border: 1px solid #fff;
+    color: #fff;
+    display: inline-flex;
+    height: 40px;
+    justify-content: center;
+    margin-right: 10px;
+    text-decoration: none;
+    width: 40px;
+  }
+`
